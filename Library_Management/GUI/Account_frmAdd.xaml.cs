550874730fe1f7b041caa7493de8c9bf7ae4c429
ThaiVinh <18521381@gm.uit.edu.vn>
2020-06-07 11:38:00 +0700

    Account_frmAdd

---
+++ Library_Management/GUI/Account_frmAdd.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library_Management.GUI
+{
+    /// <summary>
+    /// Interaction logic for Account_frmAdd.xaml
+    /// </summary>
+    public partial class Account_frmAdd : Grid
+    {
+        public Account_frmAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Search_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
